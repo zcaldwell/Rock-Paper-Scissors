fb0283919786f1a8d2190a1e23c5c24b08f40bae
zcaldwell <mintyst.john@gmail.com>
2021-10-06 13:30:05 -0700

    html done, moving onto light css then js

--- cypress/integration/example.spec.js
+++ cypress/integration/example.spec.js
@@ -5,8 +5,8 @@ describe('Example Test', ()=>{
     });
 
     describe('HTML elements', ()=>{   
-        it('contains the alchemy logo', ()=>{
-            cy.get('.main-logo').should('be.visible');
+        it('contains the body', ()=>{
+            cy.get('body').should('be.visible');
         });
     });
 });
